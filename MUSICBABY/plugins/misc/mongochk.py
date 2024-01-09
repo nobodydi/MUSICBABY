0983f4a6ff7502b39274ffd4bff02de77f84976a
nobodydi <149184961+nobodydi@users.noreply.github.com>
2024-01-09 22:34:33 +0530

    Update mongochk.py

--- MUSICBABY/plugins/misc/mongochk.py
+++ MUSICBABY/plugins/misc/mongochk.py
@@ -8,7 +8,7 @@ from MUSICBABY import app as MUSICBABY
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
 
 
-@BABY.on_message(filters.command("mongochk"))
+@MUSICBABY.on_message(filters.command("mongochk"))
 async def mongo_command(client, message: Message):
     if len(message.command) < 2:
         await message.reply("Please enter your MongoDB URL after the command. Example: /mongochk your_mongodb_url")

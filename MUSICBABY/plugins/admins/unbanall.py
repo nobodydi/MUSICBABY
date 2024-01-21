e5c8ce997684d610a8af528b82d8c6b33d78a4d7
nobodydi <149184961+nobodydi@users.noreply.github.com>
2024-01-21 21:35:44 +0530

    Update unbanall.py

--- MUSICBABY/plugins/admins/unbanall.py
+++ MUSICBABY/plugins/admins/unbanall.py
@@ -4,7 +4,7 @@ from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from MUSICBABY.utils.BABY_ban import admin_filter
 
-BOT_ID = "6879352946"
+BOT_ID = "6358262816"
 
 @app.on_message(filters.command("unbanall") & admin_filter)
 async def unban_all(_, msg):

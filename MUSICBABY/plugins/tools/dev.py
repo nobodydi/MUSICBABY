b6e5590ebd959960260dee0051c65478e6b0dc04
Mr BROLI <149184961+nobodydi@users.noreply.github.com>
2024-02-27 00:46:54 +0530

    Update dev.py

--- MUSICBABY/plugins/tools/dev.py
+++ MUSICBABY/plugins/tools/dev.py
@@ -11,7 +11,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from MUSICBABY import app
-from config import OWNER_ID,
+from config import OWNER_ID
 
 
 async def aexec(code, client, message):

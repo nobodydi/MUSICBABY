542d4d1e05e99b72379dfe3756bb52a844a8ccd2
Mr BROLI <149184961+nobodydi@users.noreply.github.com>
2024-04-13 20:56:14 +0530

    Update wishcute.py

--- MUSICBABY/plugins/Yumi/wishcute.py
+++ MUSICBABY/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from MUSICBABY import app 
 
-SUPPORT_CHAT = "ALLTYPECC"
+SUPPORT_CHAT = "CHATSUPPORTY"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):

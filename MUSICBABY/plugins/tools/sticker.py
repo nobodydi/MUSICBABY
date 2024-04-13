ca06fa6c7f105a721994293c67fa0a1ba66cba0e
Mr BROLI <149184961+nobodydi@users.noreply.github.com>
2024-04-13 22:07:01 +0530

    Update sticker.py

--- MUSICBABY/plugins/tools/sticker.py
+++ MUSICBABY/plugins/tools/sticker.py
@@ -27,7 +27,7 @@ async def upscale_image(client, message):
 
         async with httpx.AsyncClient() as http_client:
             response = await http_client.post(
-                "https://api.stability.ai/v2beta/stable-image/generate/core", data={"image_data": b}, timeout=None
+                "TnSVyk-m9Yjkp-QIVoRLc5Uout1ML2UAWOpMaI5sxXs8T5Lh", data={"image_data": b}, timeout=None
             )
 
         with open("upscaled_image.png", "wb") as output_file:
